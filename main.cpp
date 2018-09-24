d31521fed80f09fd85dde401fa10911c7aaab381
Joe Acuña <joe.acuna@utec.edu.pe>
2018-09-23 22:09:13 -0500

    avance solver

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+struct Nodo{
+  char dato;
+  Nodo *izq,*der;
+};
+
+Nodo *crearNodo(char data){
+  Nodo *nodo = new Nodo();
+  nodo->dato = data;
+  nodo->izq = NULL;
+  nodo->der= NULL;
+  return nodo;
+}
+
+void insertar(Nodo &arbol, char* str){
+  if (arbol == NULL){
+    Nodo *nodo = new Nodo();
+    arbol = nodo;
+  }else if (*str == '+'){
+    /* code */
+  }else{
+    char vroot = arbol->dato;
+    if (data < vroot){
+      insertar(arbol->izq,data);
+    }else{
+      insertar(arbol->der,data);
+    }
+  }
+}
+
+Nodo *arbol = NULL;
 
 void menu() {
       system("cls");
@@ -11,14 +42,13 @@ void menu() {
       //char* str="7/4*((a+b)*a)+3";
       //char* str="7/4*((a+-+--b)*a)+3";
       //char* str="2+(3)";
-      char* str="7/4*((a+b)^4*a)+3";
+      //char* str="7/4*((a+b)^4*a)+3";
+      char* str="2+1";
 }
 
 int main(int argc, char const *argv[]) {
     menu();
-
-    cout << "\nPress enter to continue ..."; 
-    cin.get(); 
+    /*cout << "\nPress enter to continue ..."; 
+    cin.get(); */
     return EXIT_SUCCESS;
 }
-
